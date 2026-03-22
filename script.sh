37e76ddbd247095326bfb698ef5ddd53ee8482c1
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-03-22 16:09:58 -0400

    update.

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 # for server:
+
+# name it whatever you want to name it. i named it serveme.
 serveme(){
 # get interface:
 IFACE=$(route get default | grep interface | awk '{print $2}')
@@ -6,16 +8,20 @@ IFACE=$(route get default | grep interface | awk '{print $2}')
 # get ip:
 IP=$(ipconfig getifaddr $IFACE)
 
+# assign port; 
+PORT=8000
+
 # fallback:
 if [ -z "$IP" ]; then
   IP="localhost"
 fi
 
 echo ""
-echo "ip to test on phone: $IP:8080"
+echo "ip to test on phone: $IP:$PORT"
 echo ""
 
-open -a "Google Chrome" "http://localhost:8080/"
+# can open any browser here. i chose google-chrome because it has dev-tools.
+open -a "Google Chrome" "http://localhost:$PORT/"
 
-python3 -m http.server 8080 --bind 0.0.0.0
+python3 -m http.server $PORT --bind 0.0.0.0
 }
